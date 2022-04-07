42ccf726c1fc93747df3dd867a414f78ac03902d
bjerkin <38444912+bjerkin@users.noreply.github.com>
2022-04-07 04:04:57 +0300

    Delete config.js

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const MOVIE_API_URL = "https://api.themoviedb.org/3/";
-export const REACT_APP_MOVIE_API_KEY = "a23e63118b26a650187a1af2ccf8ae84";
-export const MOVIE_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
