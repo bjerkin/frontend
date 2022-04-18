1a7428fbe44db0bc7e7ad246446eef1939f7f4e1
bjerkin <berkinislek@gmail.com>
2022-04-18 22:45:53 +0300

    moved navbar styling codes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
 import { SidebarData } from './SidebarData'
-import './Navbar.css'
 
 function Navbar() {
 
